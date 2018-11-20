90fa9f0cb0cbbaa1e3fda49fa2cba200f9a250e5
Diego <diego.puyol@gmail.com>
2018-11-20 20:31:49 +0100

    ButtonComponent

---
+++ App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import AppButton from './app/components/AppButton';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <AppButton
+        bgColor='gray'
+        title='Test'
+        action={()=> console.log(1)}
+        iconName='sign-in'
+        iconSize={30}
+        iconColor='#fff'
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ app/components/PreLoader.js
@@ -0,0 +1,2 @@
+import React, { Component } from 'react'
+import { ActivityIndicator, View, StyleSheet, Dimensions } from 'react-native'
